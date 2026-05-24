7a4503cdf9ad3b298b37cab0097faeda81dc89b9
Maria Isabella <araujomariaisabella@gmail.com>
2026-05-24 17:52:03 -0300

    quinto commit do js e projeto FINALIZADO

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ contaInput.addEventListener("input", receberValorConta)
 
 function receberValorConta(evento) {
    conta = Number(evento.target.value)
+
+   calcular()
 }
 
 const pessoasInput = document.querySelector("#pessoas")
@@ -24,6 +26,8 @@ function receberQuantidadePessoas(evento) {
         divErro.setAttribute("id", "")
         pessoas = Number(evento.target.value)
     }
+
+    calcular()
 }
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type= 'button']")
@@ -38,6 +42,8 @@ function receberPorcentagem(evento) {
             if(botao.value === evento.target.value) {
               botao.classList.add("botao-ativo")                  
             }
+
+            
     })
 
     if(evento.target.value !== ""){
@@ -47,11 +53,39 @@ function receberPorcentagem(evento) {
     }
 
 
-    console.log(porcentagem)
+   calcular()
 }
 
 const gorjetaInput = document.querySelector("#outra")
 gorjetaInput.addEventListener("input", receberPorcentagem)
+
+function calcular(){
+    if(conta !== 0 && porcentagem !== 0 && pessoas !== 0){
+    const strongGorjetaTotal = document.querySelector(".gorjeta-total > strong")
+    strongGorjetaTotal.innerHTML = `R$ ${(conta * porcentagem / pessoas).toFixed(2)}`
+    const strongTotal = document.querySelector(".total > strong")
+    strongTotal.innerHTML = `R$ ${((conta + (conta * porcentagem)) / pessoas).toFixed(2)}`
+
+    }
+}
+
+const botaoLimpar = document.querySelector(".resultados button") 
+botaoLimpar.addEventListener("click", limpar)
+
+function limpar(){
+    contaInput.value = ""
+
+    botoesGorjeta.forEach(botao =>{
+        botao.classList.remove("botao-ativo")
+    })
+
+    gorjetaInput.value = ""
+
+    pessoasInput.value = ""
+
+    document.querySelector(".gorjeta-total > strong").innerHTML = "R$ 0.00"
+    document.querySelector(".total > strong").innerHTML = "R$ 0.00"
+}
 
 
  
